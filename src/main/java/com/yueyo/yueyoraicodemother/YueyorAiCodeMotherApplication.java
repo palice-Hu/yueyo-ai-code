5702142b740e0ce3bb420f224a351b606c9b338d
MallChat Developer <developer@mallchat.com>
2025-11-20 23:05:15 +0800

    1. 用户模块的开发 2. 部分ai模块的开发

--- src/main/java/com/yueyo/yueyoraicodemother/YueyorAiCodeMotherApplication.java
+++ src/main/java/com/yueyo/yueyoraicodemother/YueyorAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.yueyo.yueyoraicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yueyo.yueyoraicodemother.mapper")
 public class YueyorAiCodeMotherApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yueyo/yueyoraicodemother/aop/AuthInterceptor.java
@@ -0,0 +1,59 @@
+package com.yueyo.yueyoraicodemother.aop;
+
+
+import com.yueyo.yueyoraicodemother.annotation.AuthCheck;
+import com.yueyo.yueyoraicodemother.exception.BusinessException;
+import com.yueyo.yueyoraicodemother.exception.ErrorCode;
+import com.yueyo.yueyoraicodemother.model.entity.User;
+import com.yueyo.yueyoraicodemother.model.enums.UserRoleEnum;
+import com.yueyo.yueyoraicodemother.service.UserService;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+public class AuthInterceptor {
+
+
+    @Resource
+    private UserService userService;
+
+
+    @Around("@annotation(authCheck)") //含义：拦截所有使用了AuthCheck注解的方法
+    public Object doInterceptor(ProceedingJoinPoint proceedingJoinPoint, AuthCheck authCheck) throws Throwable{
+        String mustRole = authCheck.mustRole();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request =((ServletRequestAttributes) requestAttributes).getRequest();
+
+        //获取当前接口所需的权限
+        UserRoleEnum mustRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
+
+        // 不需要权限，放行
+        if(mustRoleEnum == null) {
+            return proceedingJoinPoint.proceed();
+        }
+
+        // 判断用户权限
+        User loginUser = userService.getLoginUser(request);
+        UserRoleEnum userRoleEnum = UserRoleEnum.getEnumByValue(loginUser.getUserRole());
+
+        // 用户无任何权限
+        if(userRoleEnum == null) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+
+        // 要求管理员权限，用户无管理员权限
+        if(UserRoleEnum.ADMIN.equals(mustRoleEnum) && !UserRoleEnum.ADMIN.equals(userRoleEnum)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+
+        return proceedingJoinPoint.proceed();
+    }
+}
