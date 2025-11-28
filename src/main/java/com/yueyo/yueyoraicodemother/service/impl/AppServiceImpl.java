9d2d0e987426924a34003c697a1a36b1d2668efe
yueyor <1634297782@qq.com>
2025-11-28 15:23:57 +0800

    refactor(ai): ai多例模式，搁置

--- src/main/java/com/yueyo/yueyoraicodemother/service/impl/AppServiceImpl.java
+++ src/main/java/com/yueyo/yueyoraicodemother/service/impl/AppServiceImpl.java
@@ -8,6 +8,7 @@ import cn.hutool.core.util.StrUtil;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.yueyo.yueyoraicodemother.ai.AiCodeGenTypeRoutingService;
+import com.yueyo.yueyoraicodemother.ai.AiCodeGenTypeRoutingServiceFactory;
 import com.yueyo.yueyoraicodemother.constant.AppConstant;
 import com.yueyo.yueyoraicodemother.core.AiCodeGeneratorFacade;
 import com.yueyo.yueyoraicodemother.core.builder.VueProjectBuilder;
@@ -71,6 +72,10 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
     @Resource
     private ScreenshotService screenshotService;
 
+    @Resource
+    private AiCodeGenTypeRoutingServiceFactory aiCodeGenTypeRoutingServiceFactory;
+
+
 
     @Resource
     private AiCodeGenTypeRoutingService aiCodeGenTypeRoutingService;
@@ -87,8 +92,11 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         // 应用名称暂时为 initPrompt 前 12 位
         app.setAppName(initPrompt.substring(0, Math.min(initPrompt.length(), 12)));
         // 使用 AI 智能选择代码生成类型
-        CodeGenTypeEnum selectedCodeGenType = aiCodeGenTypeRoutingService.routeCodeGenType(initPrompt);
-        app.setCodeGenType(selectedCodeGenType.getValue());
+//        CodeGenTypeEnum selectedCodeGenType = aiCodeGenTypeRoutingService.routeCodeGenType(initPrompt);
+//        app.setCodeGenType(selectedCodeGenType.getValue());
+        // 使用 AI 智能选择代码生成类型（多例模式）
+        AiCodeGenTypeRoutingService routingService = aiCodeGenTypeRoutingServiceFactory.createAiCodeGenTypeRoutingService();
+        CodeGenTypeEnum selectedCodeGenType = routingService.routeCodeGenType(initPrompt);
         // 插入数据库
         boolean result = this.save(app);
         ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
